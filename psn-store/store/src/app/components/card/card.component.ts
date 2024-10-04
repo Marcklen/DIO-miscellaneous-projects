b082bca716614b131beb64e2c8161b4a2eb20179
marcklen <marcklen@icloud.com>
2024-10-04 14:30:35 -0300

    feat: novos components - fix: erros na importacao

--- psn-store/store/src/app/components/card/card.component.ts
+++ psn-store/store/src/app/components/card/card.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  standalone: true,
-  imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+	@Input()
+	gameCover:string = ""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string ="XPTO | PS4"
+	@Input()
+	gamePrice:string = "R$ 399,90"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
--- psn-store/store/src/app/pages/home/home.component.ts
+++ psn-store/store/src/app/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CardComponent } from "../../components/card/card.component";
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports:[CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
