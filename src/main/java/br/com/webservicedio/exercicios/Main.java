12c5b1f3f8e98ecd0741b3c403b3fae65df4702d
marcklen <marcklen@icloud.com>
2024-10-14 20:55:48 -0300

    Questao02 ok

--- src/main/java/br/com/webservicedio/exercicios/Main.java
+++ src/main/java/br/com/webservicedio/exercicios/Main.java
@@ -7,8 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         //testes OK para a questão 01
-        var questao01 = new Questao01();
-        questao01.incrementarValores(List.of(1L, 2L, 3L, 4L, 5L))
+//        var questao01 = new Questao01();
+//        questao01.incrementarValores(List.of(1L, 2L, 3L, 4L, 5L))
+//                .doOnNext(System.out::println)
+//                .subscribe();
+
+        //testes OK para a questão 02
+        var questao02 = new Questao02();
+        questao02.contarAdmins(List.of(
+                        new User(1L, "Marcklen", "marcklen@email.com", "123456789", true),
+                        new User(2L, "Monteiro", "monteiro@email.com", "123456789", false),
+                        new User(3L, "Guimaraes", "guimaraes@email.com", "123456789", true)))
                 .doOnNext(System.out::println)
                 .subscribe();
     }
