8da61138fdd2b323ff0cca559f9dcc17707daa34
marcklen <marcklen@icloud.com>
2024-10-14 20:19:39 -0300

    Commit Inicial Projeto Reator DIO

---
+++ src/test/java/br/com/webservicedio/WebserviceDioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webservicedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceDioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
