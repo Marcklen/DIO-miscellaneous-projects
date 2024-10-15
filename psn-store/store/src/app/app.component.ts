5b8d906b51b9acdae99521720b346849590f8546
marcklen <marcklen@icloud.com>
2024-10-15 10:39:11 -0300

    fix: ultimos ajustes

--- psn-store/store/src/app/app.component.ts
+++ psn-store/store/src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterModule, CommonModule]
+
 })
 export class AppComponent {
   title = 'store';
